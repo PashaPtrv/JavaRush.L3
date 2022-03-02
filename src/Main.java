c02c781e3a4ba43a7e3492950c25659422b24657
Pasha <pvlptrv@yandex.ru>
2022-03-03 02:16:05 +0300

    Лекция 3, блок 3, задание 1.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-       //Вывести на экран следующий текст - две строки:
-        //It's Windows path: "C:\Program Files\Java\jdk1.7.0\bin"
-        //It's Java string: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"
-        System.out.println("It's Windows path: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"");
-        System.out.println("It's Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk1.7.0\\\\bin\\\"");
+        //Ввести с клавиатуры число и имя, вывести на экран строку:
+        //«имя» захватит мир через «число» лет. Му-ха-ха!
 
-        //Выведи на экран 日本語
-        System.out.println("日本語");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя захватчика: ");
+        String name = scanner.nextLine();
+        System.out.println("Введите колличество лет: ");
+        int num = scanner.nextInt();
+        System.out.println(name + " захватит мир через " + num + " лет Му-ха-ха!");
+
     }
 }
 
