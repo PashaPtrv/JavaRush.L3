89ced0ed3cbd16a0552d83bf1d936bf70b240a7e
Pasha <pvlptrv@yandex.ru>
2022-03-03 02:38:45 +0300

    Лекция 3, блок 3, задание 2.

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //Ввести с клавиатуры число и имя, вывести на экран строку:
-        //«имя» захватит мир через «число» лет. Му-ха-ха!
+      Scanner scanner = new Scanner(System.in);
+        //вести с клавиатуры имя и вывести надпись: name зарабатывает $5,000. Ха-ха-ха!
+        System.out.println("Введите имя счастливчика: ");
+        String name1 = scanner.nextLine();
+        System.out.println(name1 + " зарабатывает $5,000. Ха-ха-ха!");
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите имя захватчика: ");
-        String name = scanner.nextLine();
-        System.out.println("Введите колличество лет: ");
-        int num = scanner.nextInt();
-        System.out.println(name + " захватит мир через " + num + " лет Му-ха-ха!");
 
     }
 }
