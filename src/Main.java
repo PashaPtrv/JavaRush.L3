3b83dab70c8c6bb1164936be71c30d8c5dcc8c9c
Pasha <pvlptrv@yandex.ru>
2022-03-03 01:15:37 +0300

    Лекция 3, блок 2, задание 2.

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
+       /* Выведи на экран таблицу умножения 10 на 10 в следующем виде:
+        1 2 3 …
+        2 4 6 …
+        3 6 9 … */
+        for (int i = 1; i < 11; i++) {
 
-        //Вывести на экран все возможные комбинации слов «Мама», «Мыла», «Раму».
-        //Подсказка: их 6 штук. Каждую комбинацию вывести с новой строки. Слова не разделять.
+            for (int j = 1; j < 11; j++) {
 
-        String text[] = {"Мама", "Мыла", "Раму"};
-        for (int i = 0; i < text.length; i++) {
-            for (int j = 0; j < text.length; j++) {
-                for (int k = 0; k < text.length; k++) {
-                    if (i != j && i != k && j != k) {
-                        System.out.println(text[i] + text[j] + text[k]);
-                    }
-                }
+                System.out.print(i * j + " ");
             }
+                System.out.println();
         }
-        // Так же есть более простой вариант, методом ручного перебора.
-        String a= "Мама";
-        String b= "Мыла";
-        String c= "Раму";
-
-        System.out.println(a+b+c);
-        System.out.println(a+c+b);
-        System.out.println(b+a+c);
-        System.out.println(b+c+a);
-        System.out.println(c+a+b);
-        System.out.println(c+b+a);
-
     }
 }
 
